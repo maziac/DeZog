88f08f791e3b0b53e373c2a09dc76313390cd239
maziac <thomas.busse@gmx.net>
2019-12-11 19:54:35 +0100

    More unit tests for isr.

--- src/tests/zesaruxemulator.tests.ts
+++ src/tests/zesaruxemulator.tests.ts
@@ -41,7 +41,7 @@ suite('ZesaruxEmulator', () => {
 			(<any>zSocket) = mockSocket;
 			// Push one frame on the stack
 			emul.reverseDbgStack = new RefList();
-			emul.reverseDbgStack.addObject(new Frame(0, 0, "__TEST_MAIN__"));
+			emul.reverseDbgStack.unshift(new Frame(0, 0, "__TEST_MAIN__"));
 		});
 
 		test('simple step back first history instruction', () => {
@@ -60,7 +60,7 @@ suite('ZesaruxEmulator', () => {
 			//  80D3 LD A,02h
 			const currentLine = "PC=80d3 SP=83fb AF=3f08 BC=0000 HL=4000 DE=2000 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=0d IM0 IFF12 (PC)=3e020603 (SP)=80f5";
 			// 80D5 LD B,03h (from history)
-			const prevLine = "PC=80d5 SP=83fb AF=0208 BC=0000 HL=4000 DE=2000 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=0e IM0 IFF12 (PC)=06030e04 (SP)=80f5";
+			const prevLine = "PC=80d5 SP=83fb AF=0208 BC=0000 HL=4000 DE=2000 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=0e IM0 IFF12 "; //(PC)=06030e04 (SP)=80f5";
 
 			// Handle step back
 			emul.handleReverseDebugStackBack(currentLine, prevLine);
@@ -77,7 +77,7 @@ suite('ZesaruxEmulator', () => {
 			// 80ED PUSH 2F01h
 			// 80E9 PUSH CA00h
 			const currentLine = "PC=80e9 SP=8401 AF=3f08 BC=0000 HL=4000 DE=2000 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=43 IM0 IFF12 (PC)=ed8a00ca (SP)=0065";
-			const prevLine = "PC=80ed SP=83ff AF=3f08 BC=0000 HL=4000 DE=2000 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=45 IM0 IFF12 (PC)=ed8a012f (SP)=00ca";
+			const prevLine = "PC=80ed SP=83ff AF=3f08 BC=0000 HL=4000 DE=2000 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=45 IM0 IFF12 "; //(PC)=ed8a012f (SP)=00ca";
 
 			// Handle step back
 			emul.handleReverseDebugStackBack(currentLine, prevLine);
@@ -104,6 +104,9 @@ suite('ZesaruxEmulator', () => {
 		});
 
 		test('step back call', () => {
+			// Add something to remove
+			emul.reverseDbgStack.unshift(new Frame(0, 0, "FUNC"));
+
 			// 80D3 LD A,02h
 		    // 80F2 CALL 80D3h
 			const currentLine = "PC=80f2 SP=83fd AF=3f08 BC=0000 HL=4000 DE=2000 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=3b IM0 IFF12 (PC)=cdd380c1 (SP)=0303";
@@ -112,16 +115,16 @@ suite('ZesaruxEmulator', () => {
 			// Handle step back
 			emul.handleReverseDebugStackBack(currentLine, prevLine);
 			// Value has been removed from the callstack
-			assert.equal(0, emul.reverseDbgStack.length);
+			assert.equal(1, emul.reverseDbgStack.length);
 		});
 
 		test('step back ret', () => {
 			// 80F5 POP BC
 			// 80E4 RET
 			const currentLine = "PC=80e4 SP=83fb AF=0208 BC=0304 HL=4000 DE=0202 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=48 IM0 IFF12 (PC)=c9ed8a01 (SP)=80f5";
-			const prevLine = "PC=80f5 SP=83fd AF=0208 BC=0304 HL=4000 DE=0202 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=49 IM0 IFF12 (PC)=c1d1e100 (SP)=0303";
+			const prevLine = "PC=80f5 SP=83fd AF=0208 BC=0304 HL=4000 DE=0202 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=49 IM0 IFF12 "; //(PC)=c1d1e100 (SP)=0303";
 
-			// Name of function
+			// Caller
 			mockSocket.dataArray.push("CD3412");	// memory content at CALL nnnn, e.g. CD
 
 			// Handle step back
@@ -133,34 +136,52 @@ suite('ZesaruxEmulator', () => {
 			assert.equal("1234h", frame.name);
 		});
 
-		test('step back ?', () => {
-			//  80D3 LD A,02h
-			const currentLine = "";
-			// 80D5 LD B,03h
-			const prevLine = "";
+		test('step back from isr', () => {
+			// Add something to remove
+			emul.reverseDbgStack.unshift(new Frame(0, 0, "ISR"));
+
+			// 0038 DI
+			// 80D3 LD A,02h
+			const currentLine = "PC=80d3 SP=83fb AF=0208 BC=0303 HL=0101 DE=0202 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=34 IM0 IFF12 (PC)=3e020603 (SP)=80f5";
+			const prevLine = "PC=0038 SP=83f9 AF=0208 BC=0303 HL=0101 DE=0202 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=36 IM0 IFF-- "; //(PC)=f3dde5e5 (SP)=80d5";
 
 			// Handle step back
 			emul.handleReverseDebugStackBack(currentLine, prevLine);
-			// Nothing has been pushed on the stack
-			assert.equal(0, emul.reverseDbgStack.length);
+			// Value has been removed from the callstack
+			assert.equal(1, emul.reverseDbgStack.length);
 		});
 
+		test('step back from isr to PUSH instruction', () => {
+			// Add something to remove
+			emul.reverseDbgStack.unshift(new Frame(0, 0, "FUNC"));
+
+			// 0038 DI
+			// 80E5 PUSH 0101h
+			const currentLine = "PC=80e5 SP=8403 AF=0208 BC=0303 HL=0101 DE=0202 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=5f IM0 IFF12 (PC)=ed8a0101 (SP)=8148";
+			const prevLine = "PC=0038 SP=83ff AF=0208 BC=0303 HL=0101 DE=0202 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=62 IM0 IFF-- "; //(PC)=f3dde5e5 (SP)=80e9";
+
+			// Handle step back
+			emul.handleReverseDebugStackBack(currentLine, prevLine);
+			// Value has been removed from the callstack
+			assert.equal(1, emul.reverseDbgStack.length);
+		});
 
 		test('step back into isr', () => {
-			const emul = new ZesaruxEmulator();
-			(<any>emul).cpuhistory = new ZesaruxCpuHistory();
+			// 80E9 PUSH 0202h
+			// 0049 RET
+			const currentLine = "PC=0049 SP=83ff AF=0208 BC=0303 HL=0101 DE=0202 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=7a IM0 IFF12 (PC)=c90608af (SP)=80e9";
+			const prevLine = "PC=80e9 SP=8401 AF=0208 BC=0303 HL=0101 DE=0202 IX=ffff IY=5c3a AF'=0044 BC'=0001 HL'=f3f3 DE'=0001 I=00 R=7b IM0 IFF12 "; //(PC)=ed8a0202 (SP)=0101";
 
-
-			// "ret" from an isr
-			const currentLine = "PC=0049 SP=83ff AF=0208 BC=012f HL=0065 DE=00ca IX=ffff IY=5c3a AF'=0044 BC'=174b HL'=107f DE'=0006 I=00 R=7f IM0 IFF12 (PC)=c90608af (SP)=80e9";
-			// PUSH CA00
-			const prevLine = "PC=80e9 SP=8401 AF=0208 BC=012f HL=0065 DE=00ca IX=ffff IY=5c3a AF'=0044 BC'=174b HL'=107f DE'=0006 I=00 R=00  F=----3--- F'=-Z---P-- MEMPTR=0000 IM0 IFF12 VPS: 0";
+			// There is no caller, but some memory must be returned
+			mockSocket.dataArray.push("AA3412");
 
 			// Handle step back
 			(<any>emul).handleReverseDebugStackBack(currentLine, prevLine);
-			// The interrupt should have been found
-
-			assert.equal("00123456", "2");
+			// Value has been pushed to the callstack
+			assert.equal(2, emul.reverseDbgStack.length);
+			const frame = emul.reverseDbgStack[0];
+			assert.equal(0x0049, frame.addr);
+			assert.equal("__INTERRUPT__", frame.name);
 		});
 
 	});
