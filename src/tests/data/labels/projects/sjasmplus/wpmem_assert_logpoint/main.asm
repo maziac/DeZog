706608708b5bbb6db98499cb80ed8576b7a48a9a
maziac <thomas.busse@gmx.net>
2020-09-18 19:44:18 +0200

    Updated test projects.

--- src/tests/data/labels/projects/sjasmplus/wpmem_assert_logpoint/main.asm
+++ src/tests/data/labels/projects/sjasmplus/wpmem_assert_logpoint/main.asm
@@ -1,15 +1,31 @@
 
 	ORG 0xA000
 
+	MACRO MEMGUARD
+    defs 1 ; WPMEM
+    ENDM
+
+
 	defs 0x10		; WPMEM
 	defs 0x10		; WPMEM, 5, w
 	; WPMEM 0x7000, 10,  r
 	; This is a watchpoint WPMEM 0x6000, 5,  w, A == 0
 	; WPMEMx 0x9000, 5,  w, A == 0
 
+	; This should not work: no address and no used bytes: WPMEM
+
+	ORG 0xA040
+	MEMGUARD
+	MEMGUARD
+
+	ORG 0xA100
+
 	nop		; ASSERT
 	nop		; ASSERT B==1
 	nop		; ASSERTx
+
+
+	ORG 0xA200
 
 	nop		; LOGPOINT [GROUP1] ${A}
 	nop		; LOGPOINT [GROUP1] BC=${hex:BC}
--- src/tests/labels_wpmem_assert_logpoint.tests.ts
+++ src/tests/labels_wpmem_assert_logpoint.tests.ts
@@ -15,15 +15,23 @@ suite('Labels (WPMEM, ASSERT, LOGPOINT)', () => {
 		Labels.readListFiles(config);
 
 		const wpmemLines=Labels.getWatchPointLines();
-		assert.equal(wpmemLines.length, 4);
-		assert.equal(wpmemLines[0].address, 0xA000);
+		assert.equal(wpmemLines.length, 8);
+		assert.equal(wpmemLines[0].address, undefined);
 		assert.equal(wpmemLines[0].line, "WPMEM");
-		assert.equal(wpmemLines[1].address, 0xA010);
-		assert.equal(wpmemLines[1].line, "WPMEM, 5, w");
-		assert.equal(wpmemLines[2].address, undefined);
-		assert.equal(wpmemLines[2].line, "WPMEM 0x7000, 10,  r");
+		assert.equal(wpmemLines[1].address, 0xA000);
+		assert.equal(wpmemLines[1].line, "WPMEM");
+		assert.equal(wpmemLines[2].address, 0xA010);
+		assert.equal(wpmemLines[2].line, "WPMEM, 5, w");
 		assert.equal(wpmemLines[3].address, undefined);
-		assert.equal(wpmemLines[3].line, "WPMEM 0x6000, 5,  w, A == 0");
+		assert.equal(wpmemLines[3].line, "WPMEM 0x7000, 10,  r");
+		assert.equal(wpmemLines[4].address, undefined);
+		assert.equal(wpmemLines[4].line, "WPMEM 0x6000, 5,  w, A == 0");
+		assert.equal(wpmemLines[5].address, undefined);
+		assert.equal(wpmemLines[5].line, "WPMEM");
+		assert.equal(wpmemLines[6].address, 0xA040);
+		assert.equal(wpmemLines[6].line, "WPMEM");
+		assert.equal(wpmemLines[7].address, 0xA041);
+		assert.equal(wpmemLines[7].line, "WPMEM");
 	});
 
 	test('ASSERT', () => {
@@ -33,9 +41,9 @@ suite('Labels (WPMEM, ASSERT, LOGPOINT)', () => {
 
 		const assertLines=Labels.getAssertLines();
 		assert.equal(assertLines.length, 2);
-		assert.equal(assertLines[0].address, 0xA020);
+		assert.equal(assertLines[0].address, 0xA100);
 		assert.equal(assertLines[0].line, "ASSERT");
-		assert.equal(assertLines[1].address, 0xA021);
+		assert.equal(assertLines[1].address, 0xA101);
 		assert.equal(assertLines[1].line, "ASSERT B==1");
 	});
 
@@ -46,10 +54,10 @@ suite('Labels (WPMEM, ASSERT, LOGPOINT)', () => {
 
 		const logpointLines=Labels.getLogPointLines();
 		assert.equal(logpointLines.length, 4);
-		assert.equal(logpointLines[0].address, 0xA023);
-		assert.equal(logpointLines[1].address, 0xA024);
-		assert.equal(logpointLines[2].address, 0xA025);
-		assert.equal(logpointLines[3].address, 0xA026);
+		assert.equal(logpointLines[0].address, 0xA200);
+		assert.equal(logpointLines[1].address, 0xA201);
+		assert.equal(logpointLines[2].address, 0xA202);
+		assert.equal(logpointLines[3].address, 0xA203);
 	});
 
 
