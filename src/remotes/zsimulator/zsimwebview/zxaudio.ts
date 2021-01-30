a7fc8bb1fff8289ac92a958b4e61081267141138
maziac <thomas.busse@gmx.net>
2021-01-30 14:53:14 +0100

    Silence without crack.

--- src/remotes/zsimulator/zsimwebview/zxaudio.ts
+++ src/remotes/zsimulator/zsimwebview/zxaudio.ts
@@ -46,15 +46,19 @@ export class ZxAudio {
 	protected bufferedCount = 0;
 
 	/**
-	 * Constructor.
+	 * Constructor. 
 	 */
 	constructor(sampleRate: number) {
+		sampleRate = 22050;
 		this.volume = 1.0;
 		this.sampleRate = sampleRate;
 		this.ctx = this.createAudioContext(sampleRate);
 		this.sampleRate = this.ctx.sampleRate;	// TODO: Error if wrong?
 		this.lastBeeperSample = 1;
-		this.z80TimeOffset = (this.MIN_LATENCY+this.MAX_LATENCY)/2;
+		this.z80TimeOffset = (this.MIN_LATENCY + this.MAX_LATENCY) / 2;
+
+		this.writeBeeperSamples(undefined as any);
+		this.writeBeeperSamples(undefined as any);
 	}
 
 
@@ -81,6 +85,10 @@ export class ZxAudio {
 		this.ctx.resume();
 	}
 
+	protected nextBuffer: AudioBuffer;
+	protected fixedFrameLength: number = 22050 * 0.1;	// 100ms
+	protected nextFrame: Float32Array;
+	protected frameStartTime: number;
 
 	/**
 	 * Creates an audio frame from the beeperBuffer.
@@ -88,76 +96,61 @@ export class ZxAudio {
 	 */
 	protected markIndex = true;
 	public writeBeeperSamples(beeperBuffer: BeeperBuffer) {
+		/*
 		const bufLen = beeperBuffer.bufferLen;
 		if (bufLen == 0)
 			return; 	// No samples
-
+*/
 		// Store the start time on the first packet
-		if (this.audioCtxStartTime == undefined)
+		if (this.audioCtxStartTime == undefined) {
 			this.audioCtxStartTime = this.ctx.currentTime;
-
-		// Get start beeper value
-		const value = (beeperBuffer.startValue) ? 1 : -1;
-		this.volume = 0.5;	// TODO: REMOVE
-		let audioValue = value * this.volume;
-
-		// Create a buffer
-		const totalLength = beeperBuffer.totalLength;
-		const buffer = this.ctx.createBuffer(1, totalLength, this.sampleRate);
-		const monoChannel = buffer.getChannelData(0);
-
-		// Fill buffer
-		let k = 0;
-		const tmpBuffer = beeperBuffer.buffer;
-		for (let i = 0; i < bufLen; i++) {	// TODO: Change to 'of' loop ?
-			// Get length
-			const length = tmpBuffer[i];
-			// Set all samples to the same value
-			for (let j = length; j > 0; j--) {
-				monoChannel[k++] = audioValue;
-			}
-			// Alternate for next length
-			audioValue *= -1;
+			this.frameStartTime = this.audioCtxStartTime + this.z80TimeOffset;
 		}
 
-		// "Mark" first sample
-		if (false) {
-			monoChannel[0] = 1;
-			if (this.markIndex) {
-				monoChannel[1] = 1;
-				monoChannel[2] = 1;
-				monoChannel[3] = 1;
-			}
-			this.markIndex = !this.markIndex;
+		// Check if new packet
+		if (!this.nextBuffer) {
+			this.nextBuffer = this.ctx.createBuffer(1, this.fixedFrameLength, this.sampleRate);
+			this.nextFrame = this.nextBuffer.getChannelData(0);
+		}
+
+		// Fill buffer
+		for (let i = 0; i < this.fixedFrameLength; i++) {
+			this.nextFrame[i] = -1;
 		}
 
 		// Create audio source
 		const bufferSource = this.ctx.createBufferSource();
-		bufferSource.buffer = buffer;
+		bufferSource.buffer = this.nextBuffer;
 		bufferSource.connect(this.ctx.destination);
 
 
 		// LOG REMOVE
 		bufferSource.addEventListener('ended', () => {
+			this.writeBeeperSamples(undefined as any);
+			/*
 			this.bufferedCount--;
 			this.logPassedFrames.push({
 				endedTime: this.ctx.currentTime - this.audioCtxStartTime,
 				bufferedCount: this.bufferedCount
 			});
+			*/
 		});
 
-		// Play (in near future)
-		const frameStartTime = this.audioCtxStartTime + this.z80TimeOffset + beeperBuffer.time;
-		bufferSource.start(frameStartTime);
 
+		// Play (in near future)
+		const diffTime = this.fixedFrameLength / this.sampleRate;
+		this.frameStartTime += diffTime;
+		bufferSource.start(this.frameStartTime);
+
+		/*
 		// REMOVE
 		this.bufferedCount++;
 		this.logBuf.push({
 			bufferedCount: this.bufferedCount,
 			startTime: beeperBuffer.time,
 			bufLen: bufLen,
-			totalLength: totalLength,
-			totalLengthTime: totalLength / this.sampleRate,
+			//totalLength: totalLength,
+			//totalLengthTime: totalLength / this.sampleRate,
 		});
 
 
@@ -165,5 +158,6 @@ export class ZxAudio {
 		const currentCtxTime = this.ctx.currentTime;
 		if (frameStartTime < currentCtxTime)
 			console.log("framsStartTime lower than current time: " + (frameStartTime - currentCtxTime));
+			*/
 	}
 }
