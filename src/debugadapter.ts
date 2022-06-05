d41b44257420fc37fc890fcb78d3cbdc9a11fdbf
maziac <thomas.busse@gmx.net>
2022-06-05 18:05:49 +0200

    rc2

--- src/debugadapter.ts
+++ src/debugadapter.ts
@@ -939,8 +939,6 @@ export class DebugSessionClass extends DebugSession {
 		//const disassembleMemory = false;
 		if (disassembleMemory)	// Disassembly only if PC or call stack is unknown
 		{
-// TODO: For optimization: Remove addresses for which source files exist
-
 			// Add addresses from PC history.
 			this.addAddressesFromPcHistory(longFetchAddresses);
 
