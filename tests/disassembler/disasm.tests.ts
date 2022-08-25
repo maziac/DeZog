db6c5e245f2d9472f9ee0221f5c3f4366806e6a3
maziac <thomas.busse@gmx.net>
2022-08-25 22:14:37 +0200

    Unit tests fixed.

--- tests/disassembler/disasm.tests.ts
+++ tests/disassembler/disasm.tests.ts
@@ -3935,7 +3935,10 @@ suite('Disassembler', () => {
 		let dng: SmartDisassembler;
 		let dngNodes: Map<number, AsmNode>;
 		setup(() => {
-			dng = new SmartDisassembler(addr => undefined, addr => true, addr => addr.toString(16));
+			dng = new SmartDisassembler();
+			dng.funcGetLabel = addr => undefined;
+			dng.funcFilterAddresses = addr => true;
+			dng.funcFormatLongAddress = addr => addr.toString(16);
 			(dng as any).setSlotBankInfo(0, 0xFFFF, 0, true);
 			dng.setCurrentSlots([0]);
 			readBinFile(dng,'./tests/disassembler/projects/partition_blocks/main.bin');
@@ -4446,7 +4449,10 @@ suite('Disassembler', () => {
 		let dng: SmartDisassembler;
 		let dngNodes: Map<number, AsmNode>;
 		setup(() => {
-			dng = new SmartDisassembler(addr => undefined, addr => true, addr => addr.toString(16));
+			dng = new SmartDisassembler();
+			dng.funcGetLabel = addr => undefined;
+			dng.funcFilterAddresses = addr => true;
+			dng.funcFormatLongAddress = addr => addr.toString(16);
 			(dng as any).setSlotBankInfo(0x0000, 0x3FFF, 0, true);
 			(dng as any).setSlotBankInfo(0x4000, 0x7FFF, 1, false);
 			(dng as any).setSlotBankInfo(0x8000, 0xBFFF, 2, false);
@@ -4564,7 +4570,10 @@ suite('Disassembler', () => {
 		let dngNodes: Map<number, AsmNode>;
 		let comments: Map<number, string[]>;
 		setup(() => {
-			dng = new SmartDisassembler(addr => undefined, addr => true, addr => addr.toString(16));
+			dng = new SmartDisassembler();
+			dng.funcGetLabel = addr => undefined;
+			dng.funcFilterAddresses = addr => true;
+			dng.funcFormatLongAddress = addr => addr.toString(16);
 			(dng as any).setSlotBankInfo(0x0000, 0x1FFF, 0, true);
 			(dng as any).setSlotBankInfo(0x2000, 0x3FFF, 1, false);
 			(dng as any).setSlotBankInfo(0x4000, 0x5FFF, 2, true);
