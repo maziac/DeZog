fb8dd3b6650e75e12cadeada622b4dcfb24c0e9f
maziac <thomas.busse@gmx.net>
2022-12-29 13:20:51 +0100

    zsim simulation webview initialization mainly moved to main.ts.

--- src/remotes/zsimulator/zsimwebview/main.ts
+++ src/remotes/zsimulator/zsimwebview/main.ts
@@ -1,16 +1,10 @@
-
 import {vscode} from "./vscode-import";
 import {ZxAudioBeeper, zxAudioBeeper} from "./zxaudiobeeper";
 import {UlaScreen} from "./ulascreen";
 import {VisualMem} from "./visualmem";
 import {joystickObjs, initJoystickPolling} from "./joysticks";
 import {UIAPI, UiBit} from "./helper";
-import {MemorySlotsVar} from "../../../variables/shallowvar";
-import Settings from "fast-glob/out/settings";
 
-// Make sure that it is not removed by optimization.
-joystickObjs;
-initJoystickPolling;
 
 
 // HTML element used for the cpu load.
@@ -119,10 +113,424 @@ window.addEventListener('message', event => {// NOSONAR
 /** Init: Initializes parts of the simulation.
  * This depends on if the UI element is available.
  * If it is available was setup in zsimulationview.ts.
+ * @param cpuLoadInterruptRange The number of interrupts to calculate the average from. 0 to disable.
+ * @param visualMemory Enable/disable visual memory display.
+ * @param visualMemoryZxScreen Enable/disable the display of hte ZX screen markers within the visual memory.
+ * @param ulaScreen Enable/disable the ula screen display.
+ * @param zxBorderWidth The pixel width to use for the border.
+ * @param zxBeeper Enable/disable the beeper display.
+ * @param initialBeeperValue The initial value to show for the beeper.
+ * @param audioSampleRate In Hz.
+ * @param volume Number in range [0;1.0]
+ * @param zxKeyboard Enable/disable the keyboard display.
+ * @param zxInterface2Joy Enable/disable the IF2 joystick display.
+ * @param kempstonJoy Enable/disable the Kempston joystick display.
+ * @param jsCustomCode The javascript custom code to execute. Is disabled if empty string or undefined.
+ * @param customCodeDebug Enable/disable the debug area display.
+ */
+export function initSimulation(
+	cpuLoadInterruptRange: number,
+	visualMemory: boolean, visualMemoryZxScreen: boolean, banks: any,
+	ulaScreen: boolean, zxBorderWidth: number,
+	zxBeeper: boolean, initialBeeperValue: number, audioSampleRate: number, volume: number,
+	zxKeyboard: boolean, zxInterface2Joy: boolean, kempstonJoy: boolean,
+	jsCustomCode: string, customCodeDebug: boolean
+) {
+	let html = '';
+
+	// CPU Load
+	if (cpuLoadInterruptRange > 0) {
+		html += `
+			<!-- Z80 CPU load -->
+			<p>
+				<label>Z80 CPU load:</label>
+				<label id="cpu_load_id">100</label>
+				<label>%</label>
+			</p>
+			`;
+	}
+
+	// Memory Pages / Visual Memory
+	if (visualMemory) {
+		//const visualMemoryZxScreen = Settings.launch.zsim.memoryModel.includes("ZX");
+		//const slots = this.simulator.getSlots();
+		//const banks = this.simulator.memoryModel.getMemoryBanks(slots);
+		html += `
+			<!-- Visual Memory (memory activity) -->
+			<!-- Legend, Slots -->
+			<div style="position:relative; width:100%; height:4.5em;">
+				<style>
+					.border {
+						outline: 1px solid var(--vscode-foreground);
+						outline-offset: 0;
+						height:1em;
+						position:absolute;
+						text-align: center;
+					}
+					.disabled {
+						font-style: italic;
+					}
+					.slot {
+						height:2em;
+						background: gray
+					}
+					.transparent {
+						height:2em;
+						background: transparent
+					}
+				</style>
+
+				<!-- Legend -->
+				<span style="position:absolute; top: 0em; left:0%">
+					<label style="background:blue">&ensp;&ensp;</label><label>&nbsp;PROG &ensp;&ensp;</label>
+					<label style="background:yellow">&ensp;&ensp;</label><label>&nbsp;READ &ensp;&ensp;</label>
+					<label style="background:red">&ensp;&ensp;</label><label>&nbsp;WRITE</label>
+				</span>
+
+				<!-- Address labels -->
+				<label style="position:absolute; top:2em; left:0%">0x0000</label>
+				<label style="position:absolute; top:2em; left:12.5%">0x2000</label>`;
+		// ZX screen memory marker
+		if (visualMemoryZxScreen) {
+			html += `
+				<label style="position:absolute; top:1.1em; left:25%">0x4000</label>
+				<label style="position:absolute; top:1.1em; left:35.5%">0x5B00</label>`;
+		}
+		else {
+			html += `
+				<label style="position:absolute; top:2em; left:25%">0x4000</label>`;
+		}
+
+		html += `
+			<label style="position:absolute; top:2em; left:37.5%">0x6000</label>
+			<label style="position:absolute; top:2em; left:50%">0x8000</label>
+			<label style="position:absolute; top:2em; left:62.5%">0xA000</label>
+			<label style="position:absolute; top:2em; left:75%">0xC000</label>
+			<label style="position:absolute; top:2em; left:87.5%">0xE000</label>
+
+			<!-- Marker ticks -->
+			<span class="border" style="top: 3em; left:0%; height: 1.7em"></span>
+			<span class="border" style="top: 3em; left:12.5%; height:1em;"></span>`;
+		if (visualMemoryZxScreen) {
+			// ZX screen memory marker
+			html += `
+				<span class="border" style="top: 2.0em; left:25%; height:2.5em;"></span>
+				<span class="border" style="top: 2.0em; left:34.4%; height:2.5em;"></span> <!-- 0x5800 -->
+				<span class="border" style="top: 2.0em; left:35.5%; height:2.5em;"></span> <!-- 0x5B00 -->`;
+		}
+		else {
+			// ZX screen memory marker
+			html += `
+				<span class="border" style="top: 3em; left:25%; height:1em;"></span>`;
+		}
+
+		html += `
+			<span class="border" style="top: 3em; left:37.5%; height:1em;"></span>
+			<span class="border" style="top: 3em; left:50%; height:1em;"></span>
+			<span class="border" style="top: 3em; left:62.5%; height:1em;"></span>
+			<span class="border" style="top: 3em; left:75%; height:1em;"></span>
+			<span class="border" style="top: 3em; left:87.5%; height:1em;"></span>
+		`;
+		if (visualMemoryZxScreen) {
+			// Markers for display
+			html += `
+				<!-- Extra "Screen" range display -->
+				<div class="border slot" style="top:2.2em; left:25%; width:9.4%;">SCREEN</div>
+				<div class="border slot" style="top:2.2em; left:34.4%; width:1.1%;"></div>`;
+		}
+
+		html += `
+			<!-- Visual memory image, is mainly transparent and put on top -->
+			<canvas class="slot" width="256" height="1" id="visual_mem_img_id" style="image-rendering:pixelated; position:absolute; top:3.5em; left:0; width:100%;"></canvas>
+
+			<!-- Slots  2nd -->
+			`;
+
+		// TODO: exchange banks with slot-ranges
+		const count = banks.length;
+		for (let i = 0; i < count; i++) {
+			const bank = banks[i];
+			const pos = bank.start * 100 / 0x10000;
+			const width = (bank.end + 1 - bank.start) * 100 / 0x10000;
+			const add = `<div class="border" id="slot${i}_id" style="top:3.5em; left:${pos}%; width:${width}%; height: 2em">${bank.name}</div>
+			`;
+			html += add;
+		}
+
+		html += `
+			</div>
+			<br><br>
+			`;
+	}
+
+
+	// Add code for the screen
+	if (ulaScreen) {
+		html += `
+			<!-- Display the screen gif -->
+			<canvas id="screen_img_id" width="256" height="192" style="image-rendering:pixelated; border:${zxBorderWidth}px solid white; outline: 1px solid var(--vscode-foreground); width:95%; height:95%">
+			</canvas>
+			`;
+	}
+
+	// Add code for the ZX beeper
+	if (zxBeeper) {
+		html += `
+			<details open="true">
+			<summary>ZX Beeper</summary>
+			<span style="display:table-cell; vertical-align: middle">
+				<img src="assets/loudspeaker.svg" width="20em"></img>
+				&nbsp;
+			</span>
+
+			<!-- 0/1 visual output -->
+			<span id="beeper.output" style="display:table-cell; vertical-align: middle; width: 4em">${initialBeeperValue.toString()}</span>
+
+			<!-- Volume slider -->
+			<span style="display:table-cell; vertical-align: middle;">-</span>
+
+			<span style="display:table-cell; vertical-align: middle">
+				<input id="audio.volume" type="range" min="0" max="1" step="0.01" value="0" oninput="volumeChanged(parseFloat(this.value))">
+			</span>
+			<span style="display:table-cell; vertical-align: middle">+</span>
+
+			</details>
+			`;
+	}
+
+
+	// Add code for the keyboard
+	if (zxKeyboard) {
+		html += `
+			<!-- Keyboard -->
+			<details open="true">
+			<summary>ZX Keyboard</summary>
+
+			<table style="width:100%">
+
+				<tr>
+					<td id="key_Digit1" class="td_off" onClick="cellClicked(this)">1</td>
+					<td id="key_Digit2" class="td_off" onClick="cellClicked(this)">2</td>
+					<td id="key_Digit3" class="td_off" onClick="cellClicked(this)">3</td>
+					<td id="key_Digit4" class="td_off" onClick="cellClicked(this)">4</td>
+					<td id="key_Digit5" class="td_off" onClick="cellClicked(this)">5</td>
+					<td id="key_Digit6" class="td_off" onClick="cellClicked(this)">6</td>
+					<td id="key_Digit7" class="td_off" onClick="cellClicked(this)">7</td>
+					<td id="key_Digit8" class="td_off" onClick="cellClicked(this)">8</td>
+					<td id="key_Digit9" class="td_off" onClick="cellClicked(this)">9</td>
+					<td id="key_Digit0" class="td_off" onClick="cellClicked(this)">0</td>
+				</tr>
+
+				<tr>
+					<td id="key_KeyQ" class="td_off" onClick="cellClicked(this)">Q</td>
+					<td id="key_KeyW" class="td_off" onClick="cellClicked(this)">W</td>
+					<td id="key_KeyE" class="td_off" onClick="cellClicked(this)">E</td>
+					<td id="key_KeyR" class="td_off" onClick="cellClicked(this)">R</td>
+					<td id="key_KeyT" class="td_off" onClick="cellClicked(this)">T</td>
+					<td id="key_KeyY" class="td_off" onClick="cellClicked(this)">Y</td>
+					<td id="key_KeyU" class="td_off" onClick="cellClicked(this)">U</td>
+					<td id="key_KeyI" class="td_off" onClick="cellClicked(this)">I</td>
+					<td id="key_KeyO" class="td_off" onClick="cellClicked(this)">O</td>
+					<td id="key_KeyP" class="td_off" onClick="cellClicked(this)">P</td>
+				</tr>
+
+				<tr>
+					<td id="key_KeyA" class="td_off" onClick="cellClicked(this)">A</td>
+					<td id="key_KeyS" class="td_off" onClick="cellClicked(this)">S</td>
+					<td id="key_KeyD" class="td_off" onClick="cellClicked(this)">D</td>
+					<td id="key_KeyF" class="td_off" onClick="cellClicked(this)">F</td>
+					<td id="key_KeyG" class="td_off" onClick="cellClicked(this)">G</td>
+					<td id="key_KeyH" class="td_off" onClick="cellClicked(this)">H</td>
+					<td id="key_KeyJ" class="td_off" onClick="cellClicked(this)">J</td>
+					<td id="key_KeyK" class="td_off" onClick="cellClicked(this)">K</td>
+					<td id="key_KeyL" class="td_off" onClick="cellClicked(this)">L</td>
+					<td id="key_Enter" class="td_off" onClick="cellClicked(this)">ENTER</td>
+				</tr>
+
+				<tr>
+					<td id="key_ShiftLeft" class="td_off" onClick="cellClicked(this)">CAPS S.</td>
+					<td id="key_KeyZ" class="td_off" onClick="cellClicked(this)">Z</td>
+					<td id="key_KeyX" class="td_off" onClick="cellClicked(this)">X</td>
+					<td id="key_KeyC" class="td_off" onClick="cellClicked(this)">C</td>
+					<td id="key_KeyV" class="td_off" onClick="cellClicked(this)">V</td>
+					<td id="key_KeyB" class="td_off" onClick="cellClicked(this)">B</td>
+					<td id="key_KeyN" class="td_off" onClick="cellClicked(this)">N</td>
+					<td id="key_KeyM" class="td_off" onClick="cellClicked(this)">M</td>
+					<td id="key_ShiftRight" class="td_off" onClick="cellClicked(this)">SYMB. S.</td>
+					<td id="key_Space" class="td_off" onClick="cellClicked(this)">SPACE</td>
+				</tr>
+
+			</table>
+		</details>
+		`;
+	}
+
+	// Add code for the Interface 2 joysticks
+	if (zxInterface2Joy) {
+		html += `
+			<!-- ZX Interface 2 Joysticks -->
+			<details open="true">
+			<summary>ZX Interface 2 Joysticks</summary>
+
+			<table>
+				<tr>
+				<td>
+				<table style="color:black;" oncolor="red" offcolor="white">
+					<tr>
+						<td>
+							<ui-bit id="if2.joy1.fire" style="border-radius:1em;" onchange="togglePortBitNeg(this, 0xEFFE, 0x01)">F</ui-bit>
+						</td>
+						<td align="center">
+							<ui-bit id="if2.joy1.up" onchange="togglePortBitNeg(this, 0xEFFE, 0x02)">U</ui-bit>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<ui-bit id="if2.joy1.left" onchange="togglePortBitNeg(this, 0xEFFE, 0x10)">L</ui-bit>
+						</td>
+						<td style="color:var(--vscode-editor-foreground)">Joy1</td>
+						<td>
+							<ui-bit id="if2.joy1.right" onchange="togglePortBitNeg(this, 0xEFFE, 0x08)">R</ui-bit>
+						</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td align="center">
+							<ui-bit id="if2.joy1.down" onchange="togglePortBitNeg(this, 0xEFFE, 0x04)">D</ui-bit>
+						</td>
+					</tr>
+				</table>
+			</td>
+
+			<td></td>
+			<td></td>
+
+			<td>
+				<table style="color:black;">
+					<tr>
+						<td>
+							<ui-bit id="if2.joy2.fire" style="border-radius:1em;" onchange="togglePortBitNeg(this, 0xF7FE, 0x10)">F</ui-bit>
+						</td>
+						<td align="center">
+							<ui-bit id="if2.joy2.up" onchange="togglePortBitNeg(this, 0xF7FE, 0x08)">U</ui-bit>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<ui-bit id="if2.joy2.left" onchange="togglePortBitNeg(this, 0xF7FE, 0x01)">L</ui-bit>
+						</td>
+						<td style="color:var(--vscode-editor-foreground)">Joy2</td>
+						<td>
+							<ui-bit id="if2.joy2.right" onchange="togglePortBitNeg(this, 0xF7FE, 0x02)">R</ui-bit>
+						</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td align="center">
+							<ui-bit id="if2.joy2.down" onchange="togglePortBitNeg(this, 0xF7FE, 0x04)">D</ui-bit>
+						</td>
+					</tr>
+				</table>
+				</td>
+				</tr>
+			</table>
+
+			</details>
+			`;
+	}
+
+
+	// Add code for the Kempston joystick
+	if (kempstonJoy) {
+		html += `
+			<!-- Kempston Joystick -->
+			<details open="true">
+			<summary>Kempston Joystick</summary>
+
+			<table>
+				<tr>
+				<td>
+				<table style="color:black;" oncolor="red" offcolor="white" >
+					<tr>
+						<td>
+							<ui-bit id="kempston.joy1.fire" style="border-radius:1em;color:black;" onchange="togglePortBit(this, 0x001F, 0x10)">F</ui-bit>
+						</td>
+						<td>
+							<ui-bit id="kempston.joy1.up" onchange="togglePortBit(this, 0x001F, 0x08)">U</ui-bit>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<ui-bit id="kempston.joy1.left" onchange="togglePortBit(this, 0x001F, 0x02)">L</ui-bit>
+						</td>
+						<td></td>
+						<td>
+							<ui-bit id="kempston.joy1.right" onchange="togglePortBit(this, 0x001F, 0x01)">R</ui-bit>
+						</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td>
+							<ui-bit id="kempston.joy1.down" onchange="togglePortBit(this, 0x001F, 0x04)">D</ui-bit>
+						</td>
+					</tr>
+				</table>
+				</td>
+
+				</table>
+				</td>
+				</tr>
+			</table>
+
+			</details>
+			`;
+	}
+
+	// Space for logging
+	html += `
+		<p id="log"></p>
+		`;
+
+	// Custom javascript code area
+	if (jsCustomCode) {
+		html += `
+			<!-- Room for extra/user editable javascript/html code -->
+			<p>
+				<div id="js_code_id">
+				${jsCustomCode}
+				</div>
+			</p>
+			`;
+	}
+
+	if (customCodeDebug) {
+		html += `
+			<!-- Debug Area -->
+			<hr>
+
+			<details open="true">
+				<summary>Debug Area</summary>
+				<button onclick="reloadCustomLogicAndUi()">Reload Custom Logic and UI</button>
+				&nbsp;&nbsp;
+				<button onclick="copyHtmlToClipboard()">Copy all HTML to clipboard</button>
+			</details>
+			`;
+	}
+
+	document.body.innerHTML = html;
+
+	// Connect visuals with simulation
+	connectVisuals(audioSampleRate, volume);
+}
+
+
+
+/** Connect the visuals with the simulation.
  * @param audioSampleRate In Hz.
  * @param volume Number in range [0;1.0]
  */
-export function initSimulation(audioSampleRate: number, volume: number) {
+function connectVisuals(audioSampleRate: number, volume: number) {
+
 	// Store the cpu_load_id
 	const cpuLoad = document.getElementById("cpu_load_id") as HTMLLabelElement;
 	if (cpuLoad) {
@@ -164,10 +572,10 @@ export function initSimulation(audioSampleRate: number, volume: number) {
 	}
 
 	// Joysticks (Interface II)
-	const joy1Fire = document.getElementById("if2.joy1.fire") as UiBit;
-	if (joy1Fire) {
+	const if2Joy1Fire = document.getElementById("if2.joy1.fire") as UiBit;
+	if (if2Joy1Fire) {
 		joystickObjs.push({
-			fire: joy1Fire,
+			fire: if2Joy1Fire,
 			up: document.getElementById("if2.joy1.up") as UiBit,
 			left: document.getElementById("if2.joy1.left") as UiBit,
 			right: document.getElementById("if2.joy1.right") as UiBit,
@@ -180,8 +588,23 @@ export function initSimulation(audioSampleRate: number, volume: number) {
 			right: document.getElementById("if2.joy2.right") as UiBit,
 			down: document.getElementById("if2.joy2.down") as UiBit
 		});
-
 	}
+
+	// Joystick (Kempston)
+	const kempstonJoy1Fire = document.getElementById("kempston.joy1.fire") as UiBit;
+	if (kempstonJoy1Fire) {
+		joystickObjs.push({
+			fire: kempstonJoy1Fire,
+			up: document.getElementById("kempston.joy1.up") as UiBit,
+			left: document.getElementById("kempston.joy1.left") as UiBit,
+			right: document.getElementById("kempston.joy1.right") as UiBit,
+			down: document.getElementById("kempston.joy1.down") as UiBit,
+		});
+	}
+
+	// Stat joystick polling (if joystick is setup)
+	initJoystickPolling();
+
 }
 
 
@@ -299,5 +722,3 @@ function keyup(e) {
 	const cell = findCell(e.code);
 	cellSelect(cell, false);
 }
-
-
