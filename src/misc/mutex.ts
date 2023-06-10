f39a61c1c4c9a1538d7e1b1c59ef6a4501f74c37
maziac <thomas.busse@gmx.net>
2023-06-10 15:46:30 +0200

    mutex with timeout.

--- src/misc/mutex.ts
+++ src/misc/mutex.ts
@@ -1,7 +1,6 @@
 
 /** An implementation of a mutex for typescript.
  * Usage:
- *
  * protected mutex = new Mutex();
  *
  * func() {
@@ -9,11 +8,32 @@
  * 	...
  * 	mutex.unlock();
  * }
+ *
+ * Timeout/Exceptions:
+ * The constructor can receive a time for a timeout.
+ * If it exceeds (i.e. if the time between lock/unlock is too big)
+ * an exception is thrown. But only in the event loop.
+ * I.e. it is only visible in the debug log.
+ * But any waiting lock will receive an exception in the 'await mutex.lock()' call.
  */
 export class Mutex {
 
 	// Array of resolve, reject functions
-	private queue: Array<{resolve: (value: void | PromiseLike<void>) => void, reject: (reason?: any) => void}> = [];
+	private locks: Array<{resolve: (value: void | PromiseLike<void>) => void, reject: (reason?: any) => void}> = [];
+
+	// Timeout handling
+	protected timeout: number;
+	protected timeoutHandle: NodeJS.Timeout | undefined;
+
+
+	/** Constructor.
+	 * @param timeout The time (in ms) until an error is thrown if the resource is not freed.
+	 * I.e. the maximum allowed time between calling 'lock' and 'unlock'.
+	 */
+	constructor(timeout = 5000) {
+		this.timeout = timeout;
+	}
+
 
 	/** Locks the mutex.
 	 * If no other lock exists it returns immediately.
@@ -22,11 +42,11 @@ export class Mutex {
 	public async lock(): Promise<void> {
 		return new Promise<void>((resolve, reject) => {
 			// Remember
-			this.queue.push({resolve, reject});
+			this.locks.push({resolve, reject});
 			// Check if we need to wait on others
-			if (this.queue.length === 1) {
+			if (this.locks.length === 1) {
 				// No-one to wait on, handle immediately
-				resolve();
+				this.proceed(resolve, reject);
 			}
 		});
 	}
@@ -38,13 +58,49 @@ export class Mutex {
 	 * program execution will continue there at the next event loop possibility.
 	 */
 	public unlock() {
+		if (this.timeoutHandle === undefined) {
+			throw new Error("Mutex: 'unlock' called without calling 'lock'.");
+		}
+		// Stop timer
+		clearTimeout(this.timeoutHandle);
+		this.timeoutHandle = undefined;
 		// Throw away last item
-		this.queue.shift();
+		this.locks.shift();
 		// Get next mutex item
-		const item = this.queue.at(0);
+		const item = this.locks.at(0);
 		if (item?.resolve) {
 			// Process next possible time
-			setImmediate(item.resolve);
+			this.proceed(item.resolve, item.reject);
 		}
+	}
+
+
+	/** Calls 'resolve' and starts a timer.
+	 * If timer expires before 'unlock' is called an exception is thrown.
+	 * @param resolve The resolve function, to continue.
+	 * @param reject The reject function to throw the error.
+	 */
+	protected proceed(resolve: (value: void | PromiseLike<void>) => void, reject: (reason?: any) => void) {
+		// Start rejection timer
+		this.timeoutHandle = setTimeout(() => {
+			this.locks.shift();
+			// Also reject all other locks.
+			for (const item of this.locks) {
+				item.reject(`Mutex: timeout (${this.timeout} ms) reached. 'unlock' not called in time by other lock.`);
+			}
+			// Clear list
+			this.locks = [];
+			// Throw error
+			this.throwError(`Mutex: timeout (${this.timeout} ms) reached. 'unlock' not called in time by this lock.`);
+		}, this.timeout);
+		// Proceed (run resolve)
+		setImmediate(resolve);
+	}
+
+	/** Throws an error.
+	 * Is an own function to ease unit testing.
+	 */
+	protected throwError(text: string) {
+		throw new Error(text);
 	}
 }
--- tests/mutex.tests.ts
+++ tests/mutex.tests.ts
@@ -6,6 +6,10 @@ import {Mutex} from '../src/misc/mutex';
 
 suite('Mutex', () => {
 
+	async function pause(ms: number): Promise<void> {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
 	suite('lock/unlock', () => {
 
 		test('normal: 1', async () => {
@@ -38,10 +42,16 @@ suite('Mutex', () => {
 					setImmediate(() => {
 						(async () => {
 							// Other lock
-							if (sequence++ !== 0) reject();
+							if (sequence++ !== 0) {
+								reject();
+								return;
+							}
 							await mtx.lock();	// Should block
 							// Unlock 2nd (all) locks
-							if (sequence++ !== 2) reject();
+							if (sequence++ !== 2) {
+								reject();
+								return;
+							}
 							mtx.unlock();
 							// Lock again (should not lock)
 							await mtx.lock();
@@ -58,8 +68,85 @@ suite('Mutex', () => {
 				})();
 			});
 		});
+	});
 
+	suite('timeout', () => {
+
+		test('unlock not called', async () => {
+			return new Promise<void>((resolve, reject) => {
+				(async () => {
+					await (async () => {
+						const mtx = new Mutex(10) as any;
+						let exceptionthrown = false;
+						mtx.throwError = (text: string) => {
+							exceptionthrown = true;
+						};
+						await mtx.lock();
+						if (mtx.locks.length === 0) {
+							reject();
+							return;
+						}
+						await pause(40);
+						// Check that array has been cleared and exception was thrown
+						if (mtx.locks.length === 0 && exceptionthrown)
+							resolve();
+						else
+							reject();
+					})();
+				})();
+			})
+		});
+
+		test('exception in the other lock', async () => {
+			return new Promise<void>((resolve, reject) => {
+				(async () => {
+					await (async () => {
+						const mtx = new Mutex(10) as any;
+						let exceptionthrown = false;
+						mtx.throwError = (text: string) => {
+							exceptionthrown = true;
+						};
+						await mtx.lock();
+						let sequence = 0;
+						setImmediate(() => {
+							(async () => {
+								// Other lock
+								if (sequence++ !== 0) {
+									reject();
+									return;
+								}
+								await mtx.lock();	// is never unlocked -> times out
+							})();
+						});
+						setImmediate(() => {
+							(async () => {
+								// Second lock
+								if (sequence++ !== 1) {
+									reject();
+									return;
+								}
+								try {
+									await mtx.lock();
+								}
+								catch {
+									if (sequence++ !== 2) {
+										reject();
+										return;
+									}
+									// An exception is OK
+									if (exceptionthrown)
+										resolve();
+									else
+										reject();
+									return;
+								}
+								// No exception is an error
+								reject();
+							})();
+						});
+					})();
+				})();
+			})
+		});
 	});
 });
-
-
